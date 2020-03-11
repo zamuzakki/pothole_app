db901c007543a4bdea09dcb4ae8321a775f15dae
Zulfikar Akbar Muzakki <age.zakki@gmail.com>
2020-03-11 11:30:57 +0700

    Add JS asset

---
+++ static/js/Leaflet.SvgShapeMarkers-gh-pages/source/SVG.js
@@ -0,0 +1,36 @@
+L.SVG.include({
+	_updateShape: function _updateShape(layer) {
+
+		var p = layer._point;
+		var s = layer._radius;
+		var shape = layer.options.shape;
+
+		if(shape === "diamond"){
+			var d = "M"+ (p.x-(Math.sqrt(2)*s))+ " "+ (p.y)+ " L " + (p.x) +" "+ (p.y-(Math.sqrt(2)*s))+ " L"  + (p.x+(Math.sqrt(2)*s)) + " " + (p.y)+ " L"  + (p.x) + " " + (p.y+(Math.sqrt(2)*s)) +" L"  + (p.x-(Math.sqrt(2)*s)) + " " + (p.y);
+			this._setPath(layer, d);
+		}
+		if(shape === "square"){
+			var d = "M"+ (p.x-s)+ " "+ (p.y-s)+ " L " + (p.x+s) +" "+ (p.y-s)+ " L"  + (p.x+s) + " " + (p.y+s)+ " L"  + (p.x-s) + " " + (p.y+s) +" L"  + (p.x-s) + " " + (p.y-s);
+			this._setPath(layer, d);
+		}
+		if (shape === "triangle" || shape === "triangle-up") {
+			var d = "M" + (p.x - (13/10*s)) + " " + (p.y + (0.75*s)) + " L" + (p.x) + " " + (p.y - (1.5*s)) + " L" + (p.x + (13/10*s)) + " " + (p.y + (0.75*s)) + " Z";
+			this._setPath(layer, d);
+		}
+		if (shape === "triangle-down") {
+			var d = "M" + (p.x - (13/10*s)) + " " + (p.y - (0.75*s)) + " L" + (p.x) + " " + (p.y + (1.5*s)) + " L" + (p.x + (13/10*s)) + " " + (p.y - (0.75*s)) + " Z";
+			this._setPath(layer, d);
+		}
+		if (shape === "circle") {
+			this._updateCircle(layer)
+		}
+		if (shape === "x") {
+			s = s / 2;
+			var d = 'M' + (p.x + s) + ',' + (p.y + s) +
+				'L' + (p.x - s) + ',' + (p.y - s) +
+				'M' + (p.x - s) + ',' + (p.y + s) +
+				'L' + (p.x + s) + ',' + (p.y - s);
+			this._setPath(layer, d);
+		}
+	}
+});
